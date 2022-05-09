c4db7ffb41a21319c1a2a95381ada869661ff19c
parkershamblin <parkershamblin@gmail.com>
2022-05-09 17:46:50 -0400

    added heart icon

--- lib/main.dart
+++ lib/main.dart
@@ -2,50 +2,47 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
-// import the english_words package
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+// #docregion MyApp
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // #docregion build
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Startup Name Generator'),
         ),
-        body: Center(
+        body: const Center(
           child: RandomWords(),
         ),
       ),
     );
   }
+  // #enddocregion build
 }
+// #enddocregion MyApp
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
-
-// this class saves the list of generated word pairs, which grows
-// infinitely as the user scrolls.
+// #docregion RWS-var
 class _RandomWordsState extends State<RandomWords> {
-  // suggestions list for saving suggested word pairings.
   final _suggestions = <WordPair>[];
-  // variable for making the font size larger.
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
+  // #enddocregion RWS-var
+
+  // #docregion RWS-build
   @override
   Widget build(BuildContext context) {
+    // #docregion itemBuilder
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
       itemBuilder: /*1*/ (context, i) {
@@ -55,13 +52,34 @@ class _RandomWordsState extends State<RandomWords> {
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10)); /*4*/
         }
+
+        final alreadySaved = _saved.contains(_suggestions[index]);
+
+        // #docregion listTile
         return ListTile(
           title: Text(
             _suggestions[index].asPascalCase,
             style: _biggerFont,
           ),
+          trailing: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+          ),
         );
+        // #enddocregion listTile
       },
+      // #enddocregion itemBuilder
     );
   }
+  // #enddocregion RWS-build
+  // #docregion RWS-var
+}
+// #enddocregion RWS-var
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
 }
